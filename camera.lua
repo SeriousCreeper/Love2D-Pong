c60a0bbfaa5e6e6667c1af88412fc4cd895a233d
SeriousCreeper <seriouscreeper@gmail.com>
2025-11-19 00:13:57 -0800

    added camera

---
+++ camera.lua
@@ -0,0 +1,69 @@
+Camera = {
+    x = 0,
+    y = 0,
+    scaleX = 1,
+    scaleY = 1,
+    rotation = 0,
+    layers = {}
+}
+
+function Camera:set()
+    love.graphics.push()
+    love.graphics.rotate(-self.rotation)
+    love.graphics.scale(1 / self.scaleX, 1 / self.scaleY)
+    love.graphics.translate(-self.x, -self.y)
+end
+
+function Camera:unset()
+    love.graphics.pop()
+end
+
+function Camera:move(dx, dy)
+    self.x = self.x + (dx or 0)
+    self.y = self.y + (dy or 0)
+end
+
+function Camera:rotate(dr)
+    self.rotation = self.rotation + (dr or 0)
+end
+
+function Camera:scale(sx, sy)
+    sx = sx or 1
+    self.scaleX = self.scaleX * sx
+    self.scaleY = self.scaleY * (sy or sx)
+end
+
+function Camera:setPosition(x, y)
+    self.x = x or self.x
+    self.y = y or self.y
+end
+
+function Camera:setScale(sx, sy)
+    self.scaleX = sx or self.scaleX
+    self.scaleY = sy or self.scaleY
+end
+
+function Camera:setRotation(r)
+    self.rotation = r or self.rotation
+end
+
+function Camera:mousePosition()
+  return love.mouse.getX() * self.scaleX + self.x, love.mouse.getY() * self.scaleY + self.y
+end
+
+function Camera:newLayer(scale, func)
+  table.insert(self.layers, { draw = func, scale = scale })
+  table.sort(self.layers, function(a, b) return a.scale < b.scale end)
+end
+
+function Camera:draw()
+  local bx, by = self.x, self.y
+  
+  for _, v in ipairs(self.layers) do
+    self.x = bx * v.scale
+    self.y = by * v.scale
+    Camera:set()
+    v.draw()
+    Camera:unset()
+  end
+end
